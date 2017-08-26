8bee3dd00451a53d9d3678908cb18a54c7065e14
ryunosinfx <ryunosinfx@livedoor.com>
2017-08-27 00:40:30 +0900

    jsのファイルだけ作成2

---
+++ src/core/crypt/webcrypter.js
@@ -0,0 +1,15 @@
+import constant from './constant'
+export default class WebCrypter {
+  constructor() {
+    this.constant = constant.dbName;
+  }
+  hash(){
+
+  }
+  encrypt(){
+    
+  }
+  decrypt(){
+
+  }
+}
