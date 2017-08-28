48d1bc626382138ef4e47bb17e38f3c216a5142a
ryunosinfx <ryunosinfx@livedoor.com>
2017-08-29 00:00:01 +0900

    ハッシュストレッチ関数を作成

--- src/core/crypt/webcrypter.js
+++ src/core/crypt/webcrypter.js
@@ -3,19 +3,33 @@ import String2Buffer from './string2Buffer'
 const AES_CBC = "AES-CBC";
 const scubtleCrypto = crypto.subtle;
 const iv = crypto.getRandomValues(new Uint8Array(12));
+const algorithm = {
+  name: AES_CBC,
+  iv: String2Buffer.s2b(constant.iv);
+};
+const hashLevel = "SHA-512";
 export default class WebCrypter {
-  constructor() {
-    this.iv = String2Buffer.s2b(constant.iv);
+  constructor() {}
+  hash(dataString, salt1, salt2) {
+    return hashExecute(dataString, salt1, salt2, 0);
   }
-  hash() {}
-  //Data only String
-  encrypt(key, dataString) {
-    const algorithm = {
-      name: AES_CBC,
-      iv: this.iv
-    };
+  //　ストレッチ回数分回るよ！
+  hashExecute(dataString, salt1, salt2, count) {
+    let self = this；
+    count++;
     return new Promise((resolve, reject) => {
-      scubtleCrypto.digest('SHA-256', key).then((keyBuffer) => {
+      scubtleCrypto.digest(hashLevel, dataString).then((keyBuffer) => {
+        if (constant.strechCount > count) {
+          let nextDataString = salt1 + String2Buffer.b2Base64(keyBuffer) + salt2;
+          self.hashExecute(nextDataString, salt1, salt2, count)；
+        } else {
+          resolve(String2Buffer.b2Base64(keyBuffer));
+        }
+      }});}
+  //Data only String, you need JSON.stringify.
+  encrypt(key, dataString) {
+    return new Promise((resolve, reject) => {
+      scubtleCrypto.digest(hashLevel, key).then((keyBuffer) => {
         //buffer is binary
         let dataBuffer = String2Buffer.s2b(dataString);
         scubtleCrypto.encrypt(algorithm, keyBuffer, dataBuffer).then((result) => {
@@ -30,6 +44,23 @@ export default class WebCrypter {
         reject(e);
       });
     };}
-  decrypt() {}
+  //
+  decrypt(key, cryptBuffer) {
+    return new Promise((resolve, reject) => {
+      scubtleCrypto.digest(hashLevel, key).then((keyBuffer) => {
+        //buffer is binary
+        scubtleCrypto.decrypt(algorithm, keyBuffer, cryptBuffer).then((result) => {
+          let resultString = String2Buffer.s2b(result);
+          resolve(resultString);
+        }, (e) => {
+          console.log(e);
+          reject(e);
+        });
+
+      }, (e) => {
+        console.log(e);
+        reject(e);
+      });
+    };}
 
 }
