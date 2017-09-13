c098899790876004716c752cfe2d84c1abc85757
ryunosinfx <ryunosinfx@livedoor.com>
2017-09-14 01:56:34 +0900

    Userのログイン処理がほぼ完成。テスト実デバッグ中

--- src/core/auth/authoricatorImpl.js
+++ src/core/auth/authoricatorImpl.js
@@ -26,9 +26,11 @@ export default class AuthoricatorImple {
           String2Buffer.s2b(self.EncryptionTokenAPrefix),
           String2Buffer.base642b(hashValue)
         ]);
+        resolve(self.EncryptionTokenAKey);
       }, (e) => {
         console.log("ERROR AuthoricatorImple.constructor @make EncryptionTokenAKey");
         console.log(e);
+        resolve(e);
       });
     });
     this.promiseB = new Promise((resolve) => {
@@ -38,9 +40,11 @@ export default class AuthoricatorImple {
           String2Buffer.s2b(self.EncryptionTokenBPrefix),
           String2Buffer.base642b(hashValue)
         ]);
+        resolve(self.EncryptionTokenBKey);
       }, (e) => {
         console.log("ERROR AuthoricatorImple.constructor @make EncryptionTokenBKey");
         console.log(e);
+        resolve(e);
       });
     });
     this.promiseC = new Promise((resolve) => {
@@ -50,44 +54,46 @@ export default class AuthoricatorImple {
           String2Buffer.s2b(self.EncryptionUserIdPrefix),
           String2Buffer.base642b(hashValue)
         ]);
+        resolve(self.EncryptionUserIdKey);
       }, (e) => {
         console.log("ERROR AuthoricatorImple.constructor @make EncryptionUserIdKey");
         console.log(e);
+        resolve(e);
       });
     });
     this.osName = null;
   }
-  async waitInit() {
-    await Promise.all([this.promiseA,this.promiseB,this.promiseC]);
+  waitInit() {
+    return Promise.all([this.promiseA, this.promiseB, this.promiseC]);
   }
   // トークンはArrayBuffer
   async crateCryptKeyTokenA(passwd) {
     let now = Date.now();
     let nowArry = (new Uint8Array(String2Buffer.s2b(now))).reverse();
-    return await Webcrypter.hash(this.appName + DLMT + this.userId + DLMT + String2Buffer.b2s(nowArry.buffer) + DLMT + this.ua + DLMT + this.domain, this.dbName + DLMT, DLMT + this.appName);
+    return await this.webCrypter.hash(this.appName + DLMT + this.userId + DLMT + String2Buffer.b2s(nowArry.buffer) + DLMT + this.ua + DLMT + this.domain, this.dbName + DLMT, DLMT + this.appName);
   }
   // トークンはArrayBuffer
   async crateCryptKeyTokenB(passwd, tokenA) {
     let now = Date.now();
     let nowArry = (new Uint8Array(String2Buffer.s2b(now))).reverse();
-    return await Webcrypter.hash(this.appName + DLMT + this.userId + DLMT + String2Buffer.b2s(nowArry.buffer) + DLMT + this.ua + DLMT + this.domain + DLMT + tokenA, this.dbName + DLMT, DLMT + this.appName);
+    return await this.webCrypter.hash(this.appName + DLMT + this.userId + DLMT + String2Buffer.b2s(nowArry.buffer) + DLMT + this.ua + DLMT + this.domain + DLMT + tokenA, this.dbName + DLMT, DLMT + this.appName);
   }
   // keyはArrayBuffer
   async createStep1CrypytKey(passwd) {
-    return await Webcrypter.hash(this.appName + DLMT + this.userId + DLMT + passwd + DLMT + this.ua + DLMT + this.domain, this.dbName + DLMT, DLMT + this.appName);
+    return await this.webCrypter.hash(this.appName + DLMT + this.userId + DLMT + passwd + DLMT + this.ua + DLMT + this.domain, this.dbName + DLMT, DLMT + this.appName);
   }
-  async createTokenC(passwd){
-    return await Webcrypter.hash( this.userId + DLMT + passwd + DLMT + this.ua, this.dbName + DLMT, DLMT + this.appName);
+  async createTokenC(passwd) {
+    return await this.webCrypter.hash(this.userId + DLMT + passwd + DLMT + this.ua, this.dbName + DLMT, DLMT + this.appName);
   }
-  async createTokenD(passwd){
-    return await Webcrypter.hash( this.userId + DLMT + this.ua, this.dbName + DLMT, DLMT + this.appName);
+  async createTokenD(passwd) {
+    return await this.webCrypter.hash(this.userId + DLMT + this.ua, this.dbName + DLMT, DLMT + this.appName);
   }
-  isEqualDualAllayBuffers(bufferA,bufferB){
+  isEqualDualAllayBuffers(bufferA, bufferB) {
     return String2Buffer.b2Base64Url(bufferA) === String2Buffer.b2Base64Url(bufferB);
   }
   // keyはArrayBuffer
-  async createStep2CrypytKey(passwdHash,tokenA,tokenB) {
-    let array = new Uint8Array(String2Buffer.unitbs(passwdHash,tokenA,tokenB));
+  async createStep2CrypytKey(passwdHash, tokenA, tokenB) {
+    let array = new Uint8Array(String2Buffer.unitbs(passwdHash, tokenA, tokenB));
     return array.reverse().buffer;
   }
   // あんまり進歩はありませんが、ここでカプセル化しておく。
@@ -95,17 +101,33 @@ export default class AuthoricatorImple {
     return await this.encrypt(keyB, tokenA);
   }
   // あんまり進歩はありませんが、ここでカプセル化しておく。
-  async encrypt(keyBuffer, dataBuffer) {
-    return await Webcrypter.encrypt(keyBuffer, dataBuffer);
+  async encrypt(key, data) {
+    console.log(typeof key);
+    console.log(typeof data);
+    let keyBuffer = typeof key === "string"
+      ? String2Buffer.s2b(key)
+      : key;
+    let dataBuffer = typeof data === "string"
+      ? String2Buffer.s2b(data)
+      : data;
+    return await this.webCrypter.encrypt(keyBuffer, dataBuffer);
   }
   // あんまり進歩はありませんが、ここでカプセル化しておく。
-  async decrypt(keyBuffer, dataBuffer) {
-    return await Webcrypter.decrypt(keyBuffer, dataBuffer);
+  async decrypt(key, data) {
+    console.log(typeof key);
+    console.log(typeof data);
+    let keyBuffer = typeof key === "string"
+      ? String2Buffer.s2b(key)
+      : key;
+    let dataBuffer = typeof data === "string"
+      ? String2Buffer.s2b(data)
+      : data;
+    return await this.webCrypter.decrypt(keyBuffer, dataBuffer);
   }
   // なんか区分値テーブルも隠したくなった。
   async cratePropertieOSName() {
     let nameSeed = this.domain + DLMT + this.dbName + DLMT + this.appName + DLMT + this.userId;
-    return String2Buffer.b2Base64Url(await Webcrypter.hash(nameSeed, this.dbName + DLMT, DLMT + this.appName));
+    return String2Buffer.b2Base64Url(await this.webCrypter.hash(nameSeed, this.dbName + DLMT, DLMT + this.appName));
   }
   async init() {
     if (this.osName === null) {
