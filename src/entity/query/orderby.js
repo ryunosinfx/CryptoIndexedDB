277b6b37a51a9f5ad1d536d008767f849986ba23
ryunosinfx <ryunosinfx@livedoor.com>
2017-10-11 01:25:53 +0900

    今日出来たところまdeC

---
+++ src/entity/query/orderby.js
@@ -0,0 +1,17 @@
+
+export default class orderby {
+  constructor(columns) {
+    this.columns = columns;
+    this.isDistinct = false;
+  }
+  distinctK()[
+    this.isDdstinct = true;
+    return this;
+  ]
+  execute(selectData) {
+    if(Array.isArray(selectData)){
+      for()
+    }
+    retun this;
+  }
+}
