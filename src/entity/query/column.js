a908811405c0a7d0d32ed1555d417a854c2b2522
ryunosinfx <ryunosinfx@livedoor.com>
2017-10-16 01:06:04 +0900

    今日出来たところまdeE3

--- src/entity/query/column.js
+++
@@ -1,9 +0,0 @@
-import Condtion from './condtions/condtion'
-export default class Column extends Condtion {
-  constructor(path) {
-    this.path = path;
-  }
-  execute(record) {
-    retun record;
-  }
-}
